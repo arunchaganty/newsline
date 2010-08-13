2cc6bdb0514ab94dea0a55dc418a16afa7697742
Pranesh Srinivasan <spranesh@cse.iitm.ac.in>
2010-08-14 02:22:15 +0530

    Put spaces when encountering a non-ascii character.
    The util.unicode_to_ascii conversion function now inserts spaces
    whenever it finds non ascii characters.

--- util.py
+++ util.py
@@ -3,6 +3,7 @@ Miscellaneous Util Functions
 """
 
 import sys
+import unicodedata
 
 def Log(s, stream=sys.stderr, newline=True):
     stream.write(s)
@@ -11,5 +12,12 @@ def Log(s, stream=sys.stderr, newline=True):
     return
 
 def unicode_to_ascii(text):
-    return ''.join([c for c in text if ord(c) < 128])
+    li = []
+    for c in text:
+        if ord(c) < 128:
+            li.append(c)
+        else:
+            li.append(' ')
+
+    return ''.join(li)
 
